8eba84af7961fa207afdc4527f4e64a289fb9fe1
Alisson Macedo <alissontecnico@gmail.com>
2020-07-14 14:21:00 -0300

    feat: first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+// var app = document.getElementById("app");
+// var app2 = document.getElementById("app2");
+
+// var typewriter = new Typewriter(app, {
+//   deleteSpeed: 10,
+//   loop: true,
+// });
+
+// var typewriter2 = new Typewriter(app2, {
+//   deleteSpeed: 10,
+//   loop: true,
+// });
+
+// typewriter
+//   .changeDelay(10)
+//   .typeString("Olá eu sou Alisson Macedo!")
+//   .pauseFor(2000)
+//   .deleteAll(10)
+//   .typeString("Desenvolvedor Full Stack JavaScript")
+//   .pauseFor(1000)
+//   .deleteAll(10)
+//   .typeString("<strong>Você verá coisas incríveis por aqui!</strong>")
+//   .pauseFor(1000)
+//   .deleteChars(26)
+//   .typeString("<strong>JavaScript!</strong>")
+//   .pauseFor(1000)
+//   .deleteChars(11)
+//   .typeString("<strong>ReactJS!</strong>")
+//   .pauseFor(1000)
+//   .deleteChars(3)
+//   .typeString("<strong>-Native!</strong>")
+//   .pauseFor(1000)
+//   .deleteChars(13)
+//   .typeString("<strong>NodeJS!</strong>")
+//   .pauseFor(1000)
+//   .deleteChars(18)
+//   .typeString("<strong>E muito mais!</strong>")
+//   .pauseFor(1000)
+//   .deleteAll()
+
+//   .start();
+
+// typewriter2
+//   .changeDelay(10)
+//   .typeString("")
+//   .pauseFor(800)
+//   .typeString("É MUITO BOM VER VOCÊ POR AQUI!")
+//   .pauseFor(1000)
+//   .deleteAll(10)
+//   .typeString("<strong>Você verá coisas incríveis por aqui!</strong>")
+//   .pauseFor(1000)
+//   .deleteChars(26)
+//   .typeString("<strong>JavaScript!</strong>")
+//   .pauseFor(1000)
+//   .deleteChars(11)
+//   .typeString("<strong>ReactJS!</strong>")
+//   .pauseFor(1000)
+//   .deleteChars(3)
+//   .typeString("<strong>-Native!</strong>")
+//   .pauseFor(1000)
+//   .deleteChars(13)
+//   .typeString("<strong>NodeJS!</strong>")
+//   .pauseFor(1000)
+//   .deleteChars(18)
+//   .typeString("<strong>E muito mais!</strong>")
+//   .pauseFor(1000)
+//   .deleteAll()
+
+//   .start();
